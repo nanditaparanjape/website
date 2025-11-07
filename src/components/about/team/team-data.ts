01077023a8707123fa8a6b8db52b1248e901c7f3
Rajat Kaliani <rkaliani@uci.edu>
2025-11-06 17:48:31 -0800

    Added profile data Rajat Kaliani

--- src/components/about/team/team-data.ts
+++ src/components/about/team/team-data.ts
@@ -117,6 +117,12 @@ export const CLCHC_DATA: Profile[] = [
     linkedin: "https://www.linkedin.com/in/nathnguyen7631/",
     image: "/people/nathan-nguyen.jpg",
   },
+  {
+    name: "Rajat Kaliani",
+    role: "Developer",
+    linkedin: "https://www.linkedin.com/in/rajat-kaliani/",
+    image: "/people/RajatKaliani.jpeg",
+  },
 ];
 
 export const GCF_DATA: Profile[] = [
